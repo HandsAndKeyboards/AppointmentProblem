a537a824ac6649b6ae526440418557812c0b53e2
vany <ivev152@gmail.com>
2022-03-17 15:29:43 +0000

    реализован графический интерфейс главного окна

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,15 +1,62 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+/** ********************************************* PUBLIC ********************************************* **/
+
+MainWindow::MainWindow(QWidget * parent)
+		: QMainWindow(parent), ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+	
+	connect(ui->updateAction, &QAction::triggered, this, &MainWindow::calculateProbability);
+	connect(ui->readReferenceAction, &QAction::triggered, this, &MainWindow::showReference);
+	connect(ui->readAboutProgramAction, &QAction::triggered, this, &MainWindow::showAboutProgram);
+	connect(ui->meetFromTimeEdit, &QTimeEdit::timeChanged, this, &MainWindow::calculateProbability);
+	connect(ui->meetUntilTimeEdit, &QTimeEdit::timeChanged, this, &MainWindow::calculateProbability);
+	connect(ui->waitingTimeSpinBox, &QSpinBox::valueChanged, this, &MainWindow::calculateProbability);
+	connect(ui->threePersonsCheckBox, &QCheckBox::stateChanged, this, &MainWindow::changeAmountOfPersons);
+	connect(ui->probabilityPercentageSpinBox, &QSpinBox::valueChanged, this, &MainWindow::calculateWaitingTime);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
 }
 
+/** ****************************************** PRIVATE SLOTS ***************************************** **/
+
+/// вычисление вероятности встречи на основе времени встречи и времени ожидания
+void MainWindow::calculateProbability()
+{
+
+}
+
+/**
+ * вычисление времени ожидания на основе вероятности и времени встречи
+ * @param probability вероятность (в процентах), на основе которой вычисляется время ожидания
+ */
+void MainWindow::calculateWaitingTime(int probability)
+{
+
+}
+
+/**
+ * изменение количества персон, участвующих во встрече
+ * @param newAmount новое количество персонажей
+ */
+void MainWindow::changeAmountOfPersons(int newAmount)
+{
+
+}
+
+/// вывод справки
+void MainWindow::showReference()
+{
+
+}
+
+/// вывод окна "о программе"
+void MainWindow::showAboutProgram()
+{
+
+}
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -9,13 +9,36 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	MainWindow(QWidget * parent = nullptr);
+	~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+	Ui::MainWindow * ui;
+
+private slots:
+	/// вычисление вероятности встречи на основе времени встречи и времени ожидания
+	void calculateProbability();
+
+	/**
+	 * вычисление времени ожидания на основе вероятности и времени встречи
+	 * @param probability вероятность (в процентах), на основе которой вычисляется время ожидания
+	 */
+	void calculateWaitingTime(int probability);
+
+	/**
+	 * изменение количества персон, участвующих во встрече
+	 * @param newAmount новое количество персонажей
+	 */
+	void changeAmountOfPersons(int newAmount);
+	
+	/// вывод справки
+	void showReference();
+	
+	/// вывод окна "о программе"
+	void showAboutProgram();
 };
+
 #endif // MAINWINDOW_H
